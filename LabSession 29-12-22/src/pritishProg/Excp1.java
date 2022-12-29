d6dbfd9aae6785251d5ec401b40b10e7b56fcf74
Pritishpa02 <121220667+Pritishpa02@users.noreply.github.com>
2022-12-29 16:42:20 +0530

    Update Excp1.java

--- LabSession 29-12-22/src/pritishProg/Excp1.java
+++ LabSession 29-12-22/src/pritishProg/Excp1.java
@@ -7,12 +7,12 @@ public class Excp1
   public static void main(String[] args) 
     {
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Please enter your age: ");
+	System.out.println("Please Enter Your Age: ");
 	int age = sc.nextInt();
 	//To Throw Exception if the age is below 18
 	if(age >= 18) 
 	    {
-		System.out.println("You are eligible to vote.");
+		System.out.println("You are Eligible to vote.");
 	     }
 		//if age i above 18
 		
