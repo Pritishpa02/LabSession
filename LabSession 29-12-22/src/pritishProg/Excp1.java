d4ecf5d725b91ffc3c0523d6e081a9bc7183380a
Pritishpa02 <121220667+Pritishpa02@users.noreply.github.com>
2022-12-29 16:41:29 +0530

    Update Excp1.java

--- LabSession 29-12-22/src/pritishProg/Excp1.java
+++ LabSession 29-12-22/src/pritishProg/Excp1.java
@@ -2,23 +2,24 @@ package pritishProg;
 
 import java.util.Scanner;
 
-public class Excp1 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter your age: ");
-		int age = sc.nextInt();
-		//To Throw Exception if the age is below 18
-		if(age >= 18) {
-			System.out.println("You are eligible to vote.");
-		}
+public class Excp1 
+{
+  public static void main(String[] args) 
+    {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please enter your age: ");
+	int age = sc.nextInt();
+	//To Throw Exception if the age is below 18
+	if(age >= 18) 
+	    {
+		System.out.println("You are eligible to vote.");
+	     }
 		//if age i above 18
 		
-		else {
+		else 
+		  {
 			throw new ArithmeticException("Age is not correct");
-		}
-		sc.close();
-	}
-
-	}
-
+		   }
+	sc.close();
+  }
+}
