988f9bf3ce57e3c1008a5b1fda6c7da185cc5e9b
Pritishpa02 <121220667+Pritishpa02@users.noreply.github.com>
2022-12-29 16:46:28 +0530

    Update OddOrEven.java

--- LabSession 29-12-22/src/pritishProg/OddOrEven.java
+++ LabSession 29-12-22/src/pritishProg/OddOrEven.java
@@ -1,24 +1,29 @@
 package pritishProg;
 
-public class OddOrEven implements Runnable{
+public class OddOrEven implements Runnable
+ {
 	private int number;
-
-    public OddOrEven(int number) {
+        public OddOrEven(int number)
+    {
         this.number = number;
     }
-    public void run() {
-        if (number % 2 == 0) {
-            System.out.println(number + " is an even number.");
-        } else {
-            System.out.println(number + " is an odd number.");
+   public void run() 
+    {
+        if (number % 2 == 0)
+      {
+            System.out.println(number + " Is An Even Number.");
         }
+   else 
+     {
+       System.out.println(number + " Is An Odd Number.");
+     }
+   }
+ public static void main(String[] args) 
+   {
+      Thread t1 = new Thread(new OddOrEven(1));
+      t1.start();
+
+      Thread t2 = new Thread(new OddOrEven(2));
+      t2.start();
     }
-	public static void main(String[] args) {
-		        Thread t1 = new Thread(new OddOrEven(1));
-		        t1.start();
-
-		        Thread t2 = new Thread(new OddOrEven(2));
-		        t2.start();
-		   }
 }
-
